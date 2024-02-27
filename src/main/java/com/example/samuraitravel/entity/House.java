4b3eb762b16c0ab21f4bb29454224d7a240f53fc
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-02-27 22:41:50 +0900

    create_model

---
+++ src/main/java/com/example/samuraitravel/entity/House.java
@@ -0,0 +1,52 @@
+package com.example.samuraitravel.entity;
+
+import java.sql.Timestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "houses") // マッピングするテーブル名
+@Data // ゲッターやセッターなどを自動生成
+public class House {
+	@Id // 主キー
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 自動採番
+	@Column(name = "id") // idカラム
+	private Integer id;
+	
+	@Column(name = "name")
+    private String name;
+
+    @Column(name = "image_name")
+    private String imageName;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "price")
+    private Integer price;
+
+    @Column(name = "capacity")
+    private Integer capacity;
+
+    @Column(name = "postal_code")
+    private String postalCode;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    @Column(name = "created_at", insertable = false, updatable = false)
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at", insertable = false, updatable = false)
+    private Timestamp updatedAt;
+
+}
