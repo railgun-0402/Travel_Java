5c8e924f2cb205bc02c813070b0c3e210a7ec9d1
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-02-29 22:56:28 +0900

    pagination

--- src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
+++ src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
@@ -1,7 +1,9 @@
 package com.example.samuraitravel.controller;
 
-import java.util.List;
-
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,9 +23,10 @@ public class AdminHouseController {
 	}
 	
 	@GetMapping
-	public String index(Model model) {
-		List<House> houses = houseRepository.findAll();
-		model.addAttribute("houses", houses);
+	public String index(Model model, @PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable) {
+		// ページネーションを付与
+		Page<House> housePage = houseRepository.findAll(pageable);
+		model.addAttribute("housePage", housePage);
 		
 		return "admin/houses/index";
 	}
