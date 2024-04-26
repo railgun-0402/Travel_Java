f658fde4f587f63c1bda5554ac33cc41bb0e44ca
railgun-0402 <april-life.f-441-2011@docomo.ne.jp>
2024-04-26 15:00:13 +0900

    add search

--- src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
+++ src/main/java/com/example/samuraitravel/controller/AdminHouseController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.samuraitravel.entity.House;
 import com.example.samuraitravel.repository.HouseRepository;
@@ -23,10 +24,18 @@ public class AdminHouseController {
 	}
 	
 	@GetMapping
-	public String index(Model model, @PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable) {
+	public String index(Model model, @PageableDefault(page = 0, size = 10, sort = "id", direction = Direction.ASC) Pageable pageable, @RequestParam(name = "keyword", required = false) String keyword) {
 		// ページネーションを付与
-		Page<House> housePage = houseRepository.findAll(pageable);
+		Page<House> housePage;
+		
+		if (keyword != null && !keyword.isEmpty()) {
+			// 検索する場合は部分一致で探す
+			housePage = houseRepository.findByNameLike("%" + keyword + "%", pageable);
+		} else {
+			housePage = houseRepository.findAll(pageable);
+		}
 		model.addAttribute("housePage", housePage);
+		model.addAttribute("keyword", keyword);
 		
 		return "admin/houses/index";
 	}
